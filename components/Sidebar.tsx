9d698d6a6eedbf6df7de683e2b5e547ea3b31e40
jasir-pv <jasirahsanpv40@gmail.com>
2025-02-22 23:29:58 +0530

    feat: side bar expand added

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -2,9 +2,11 @@
 import { useState } from 'react';
 import { FaTachometerAlt, FaUserGraduate, FaChalkboardTeacher, FaMoneyBill, FaTools, FaBookOpen, FaCog, FaSignOutAlt } from 'react-icons/fa';
 import Image from 'next/image';
+import { GoSidebarExpand } from 'react-icons/go';
 
 const Sidebar = () => {
   const [active, setActive] = useState('Dashboard');
+  const [expand, setExpand] = useState(true);
 
   const menuItems = [
     { name: 'Dashboard', icon: <FaTachometerAlt /> },
@@ -16,11 +18,16 @@ const Sidebar = () => {
   ];
 
   return (
-    <div className="h-screen w-72 md:w-64 lg:w-72 bg-white rounded-3xl shadow-lg p-5 flex flex-col justify-between">
+    <div className={`h-screen ${expand ? 'w-72' : 'w-20'}  lg:w-72 bg-white rounded-3xl shadow-lg p-5 flex flex-col justify-between transition-all duration-200`}>
       <div>
-      <div className="flex items-center justify-center space-x-2 mb-8 mt-8 w-1/"  >
-          <Image src="/college-logo.svg" alt="School App" width={30} height={30} />
-          <h1 className="text-xl font-bold">School App</h1>
+        <div className="flex items-center justify-around space-x-2 mb-8 mt-8">
+          <div className="flex items-center justify-center space-x-2">
+           {expand && <Image src="/college-logo.svg" alt="School App" width={30} height={30} />}
+            {expand && <h1 className="text-xl font-bold">School App</h1>}
+          </div>
+          <button onClick={() => setExpand(!expand)}>
+            <GoSidebarExpand className='w-6 h-6 opacity-40' />
+          </button>
         </div>
 
         <div className="space-y-4 mt-14">
@@ -33,20 +40,20 @@ const Sidebar = () => {
               }`}
             >
               {item.icon}
-              <span>{item.name}</span>
+              {expand && <span>{item.name}</span>}
             </button>
           ))}
         </div>
       </div>
-      
+
       <div className="space-y-2 mb-6">
         <button className="flex items-center space-x-2 px-4 py-2 rounded-lg w-full hover:bg-gray-100">
           <FaCog />
-          <span>Settings</span>
+          {expand && <span>Settings</span>}
         </button>
         <button className="flex items-center space-x-2 px-4 py-2 rounded-lg w-full hover:bg-gray-100">
           <FaSignOutAlt />
-          <span>Log Out</span>
+          {expand && <span>Log Out</span>}
         </button>
       </div>
     </div>
